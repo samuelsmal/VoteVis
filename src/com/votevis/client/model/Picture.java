e06184daf8c4556e36ec984fecb025be3df619ce
raccc <ch.tresch@gmail.com>
2013-11-27 17:25:10 +0100

    Comment models added.

---
+++ src/com/votevis/client/model/Picture.java
@@ -0,0 +1,11 @@
+package com.votevis.client.model;
+
+import java.util.Date;
+
+public class Picture {
+	
+	//Link to local jpg;
+	String author;
+	Date date;
+
+}
--- src/com/votevis/client/presenter/CommentPresenter.java
+++ src/com/votevis/client/presenter/CommentPresenter.java
@@ -1,13 +1,22 @@
 package com.votevis.client.presenter;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasHTML;
+import com.google.gwt.user.client.ui.RichTextArea;
 import com.google.gwt.user.client.ui.Widget;
+import com.votevis.client.model.*;
+
 
 public class CommentPresenter extends Composite {
 	// Ignore this warning. It is correct.
@@ -17,9 +26,16 @@ public class CommentPresenter extends Composite {
 	
 	@UiField SpanElement titleSpan;
 	@UiField DivElement bodyDiv;
+	@UiField DivElement oldComments;
+	@UiField Button addComment;
+	@UiField Button resetComment;
+	@UiField RichTextArea textInputField; 
+	
+	CommentBase cBase;
 	
 	public CommentPresenter() {
 	    initWidget(binder.createAndBindUi(this));
+	    cBase = new CommentBase();
 	}
 	
 	public void setTitle (String title) {
@@ -29,4 +45,51 @@ public class CommentPresenter extends Composite {
 	public void setBody (String body) {
 		bodyDiv.setInnerText(body);
 	}
+	
+	
+    public HasHTML getEnteredText() { 
+            return this.textInputField; 
+    } 
+
+    public Widget asWidget() { 
+            return this; 
+    } 
+	
+	public void setOldComments (String body) {
+		oldComments.setInnerText(body);
+	}
+	
+	@UiHandler("addComment")
+	public void addComment (ClickEvent e) {
+		bodyDiv.setInnerText("bla");
+	}
+	
+	@UiHandler("resetComment")
+	public void resetComment (ClickEvent e) {
+		bodyDiv.setInnerText("blabla");
+	}
+	
+	/*
+	public void getCommentText (String body) {
+		oldComments.setInnerText(body);
+	}
+	
+	@UiHandler("commentLink")
+	public void handleComment(ClickEvent e) {
+		//content.clear();
+		/*
+		if (cBase == null) {
+			CommentBase cBase = new CommentBase();
+		}
+		Comment c = new Comment();
+		//c.setAuthor(author);
+		c.setDate(new Date());
+		//c.setComment(comment);
+		
+		cBase.getComments().add(c);
+		//content.add(vis);
+		 
+	}
+*/
+	
 }
